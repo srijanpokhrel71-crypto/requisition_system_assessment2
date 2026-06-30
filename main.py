6f3257c75fb845bc7552cb8e8c1338f8897f1a79
Srijan Pokhrel <srijanpokhrel@gmail.com>
2026-06-30 20:14:06 +1200

    Implement respond_requisition method

--- main.py
+++ main.py
@@ -61,12 +61,49 @@ class Requisition:
     def display_requisition(self):
           print("\n Requisition Details")
           print("Date:", self.date)
-          print("Staff Id:", self.date)
+          print("Staff Id:", self.staff_id)
           print("Staff Name:", self.staff_name)
           print("Requisition ID:", self.requisition_id)
           print("Total: $", self.total)
           print("Status:", self.status)
           print("Approval Reference:", self.approval_ref)
+
+
+    def respond_requisition(self):
+          if self.status == "Pending":
+                print("Manager response required")
+                print("1. Approved")
+                print("2. Not Approved")
+                print("3. Pending")
+
+                choice = input("Enter manager choice: ")
+
+                if choice == "1":
+                      self.status = "Approved"
+                      last_three = str(self.requisition_id)[-3:]
+                      self.approval_ref = self.staff_id + last_three
+
+
+                elif choice =="2":
+                      self.status = "Not Approved"
+                      self.approval_ref = "Not available"
+
+
+
+                elif choice == "3":
+                      self.status = "Pending"
+
+
+                else:
+                      print("Invalid choice")
+                    
+
+
+
+            
+          
+
+                
 
           
 
@@ -79,11 +116,21 @@ req = Requisition()
 req.add_requisition()
 req.approve_requisition()
 req.display_requisition()
+req.respond_requisition()
+
+
+
 
 
 
 print("Status:", req.status)
 print("Approval Reference:" , req.approval_ref)
+
+
+
+
+
+
 
 
 
